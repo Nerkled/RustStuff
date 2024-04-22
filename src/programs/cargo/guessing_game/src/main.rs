2555e359a3637120a81fddd56d8fd7500871ae63
Henry <henrybecker1014@gmail.com>
2024-04-21 20:58:11 -0400

    Here it is the completed not so guessing, guessing game! The newest addition to it was the cmp library, and the guess being shadowed by another guess which is using the u32 (unsigned 32 bit number) following the guess there, there are 3 methods trim, this gets rid of whitespace around a possible entry of a number

--- src/programs/cargo/guessing_game/src/main.rs
+++ src/programs/cargo/guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use std::io;    // IO input library 
+use std::cmp::Ordering; // comparison library, enum that has less, greater, equal
 use rand::Rng;  // random number generator library
 
 fn main() {
@@ -8,12 +9,30 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");   
+    println!("Please input your guess.");
 
-    let mut guess = String::new();          // variable to store the user input, also a mutable variable meaning it can change
-                                            // String:: new() is a function that returns a new mutable instance of a string
-    io::stdin()                             // type that represents a handle to the standard input for your terminal
-        .read_line(&mut guess)              // read_line method on the standard input handle to get input from the user, & indicates that this agument is a refence
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Please type a number!");
+    
+
+    // let mut guess = String::new();          // variable to store the user input, also a mutable variable meaning it can change
+    //                                         // String:: new() is a function that returns a new mutable instance of a string
+    // io::stdin()                             // type that represents a handle to the standard input for your terminal
+    //     .read_line(&mut guess)              // read_line method on the standard input handle to get input from the user, & indicates that this agument is a refence
+    //     .expect("Failed to read line");
     println!("You guessed: {guess}");       // {} placeholder for the value of the variable guess
+
+    match guess.cmp(&secret_number) {       // Mathes are made up of arms, arms consist of a pattern to match against 
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
