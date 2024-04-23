dfd8a4f0c3d6f6e0a895ba5e115c7a6e9c1af837
Henry <henrybecker1014@gmail.com>
2024-04-23 12:40:59 -0400

    Finished beginner rust guessing game, this lesson taught me some concepts such as let, match, trim and parse, functions, the use of external crates

--- src/programs/cargo/guessing_game/src/main.rs
+++ src/programs/cargo/guessing_game/src/main.rs
@@ -7,32 +7,42 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..= 100);
 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
 
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop{
+        println!("Please input your guess.");
 
-    let guess: u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number!");
-    
+        let mut guess = String::new();
 
-    // let mut guess = String::new();          // variable to store the user input, also a mutable variable meaning it can change
-    //                                         // String:: new() is a function that returns a new mutable instance of a string
-    // io::stdin()                             // type that represents a handle to the standard input for your terminal
-    //     .read_line(&mut guess)              // read_line method on the standard input handle to get input from the user, & indicates that this agument is a refence
-    //     .expect("Failed to read line");
-    println!("You guessed: {guess}");       // {} placeholder for the value of the variable guess
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {       // Mathes are made up of arms, arms consist of a pattern to match against 
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue, // undersocre acts as a catch all value
+        };  // We changed this here because we want to handle the error better instead of a crash 
+            // .trim()
+            // .parse()
+            // .expect("Please type a number!");
+        
+
+        // let mut guess = String::new();          // variable to store the user input, also a mutable variable meaning it can change
+        //                                         // String:: new() is a function that returns a new mutable instance of a string
+        // io::stdin()                             // type that represents a handle to the standard input for your terminal
+        //     .read_line(&mut guess)              // read_line method on the standard input handle to get input from the user, & indicates that this agument is a refence
+        //     .expect("Failed to read line");
+        println!("You guessed: {guess}");       // {} placeholder for the value of the variable guess
+
+        match guess.cmp(&secret_number) {       // Mathes are made up of arms, arms consist of a pattern to match against 
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
