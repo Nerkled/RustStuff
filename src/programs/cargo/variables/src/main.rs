d307a61d5f9122bf47436a365a558885322f660a
Henry <henrybecker1014@gmail.com>
2024-04-23 23:13:48 -0400

    Here some basic variables such as let and const were learned, also reinforcing what mutabmutables vs immutables are, shadowing is another topic here

--- src/programs/cargo/variables/src/main.rs
+++ src/programs/cargo/variables/src/main.rs
@@ -3,4 +3,19 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+
+    // Const code, const is always immutable and u can't change it
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // The above const name is standard naming convention in Rust
+    // Here is some helpful const stuff : https://doc.rust-lang.org/reference/const_eval.html 
+    println!("Three hours in seconds: {THREE_HOURS_IN_SECONDS}");
+
+    let y = 5; 
+
+    let y= y +1; // shadowed once, using let again here essetialy creates a whole new variable 
+    {
+        let y = y * 2;  // shadowed twice 
+        println!("The value of y in the inner scope is: {y}");
+    }
+    println!("The value of y is: {y}");
 }
